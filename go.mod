e7ad0b448e3a27668c52279c93c9afa857c0f887
xurui <1627119275@qq.com>
2021-01-12 10:59:07 +0800

    1. 更新获取的谷歌翻译的api

--- go.mod
+++ go.mod
@@ -4,16 +4,21 @@ go 1.15
 
 require (
 	github.com/gobuffalo/envy v1.9.0 // indirect
+	github.com/gobuffalo/packd v1.0.0 // indirect
 	github.com/gobuffalo/packr v1.30.1
-	github.com/gobuffalo/packr/v2 v2.8.0 // indirect
+	github.com/gobuffalo/packr/v2 v2.8.1 // indirect
 	github.com/karrick/godirwalk v1.16.1 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
 	github.com/kyai/google-translate-tk v0.0.0-20190603073310-c7f5c6d1ce08
 	github.com/rogpeppe/go-internal v1.6.2 // indirect
 	github.com/sirupsen/logrus v1.7.0 // indirect
 	github.com/spf13/cobra v1.1.1 // indirect
+	github.com/stretchr/testify v1.5.1 // indirect
 	github.com/zserge/lorca v0.1.9
-	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
-	golang.org/x/sync v0.0.0-20201008141435-b3e1573b7520 // indirect
-	golang.org/x/sys v0.0.0-20201018230417-eeed37f84f13 // indirect
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/net v0.0.0-20200226121028-0de0cce0169b // indirect
+	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a // indirect
+	golang.org/x/sys v0.0.0-20210110051926-789bb1bd4061 // indirect
+	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- tk.go
+++ tk.go
@@ -10,17 +10,17 @@ import (
 	"regexp"
 	"sync"
 )
-var LangsMap = map[string] string {"auto":"Automatic","af":"Afrikaans","sq":"Albanian","ar":"Arabic","hy":"Armenian","az":"Azerbaijani","eu":"Basque","be":"Belarusian","bn":"Bengali","bs":"Bosnian","bg":"Bulgarian","ca":"Catalan","ceb":"Cebuano","ny":"Chichewa","zh-cn":"Chinese Simplified","zh-tw":"Chinese Traditional","co":"Corsican","hr":"Croatian","cs":"Czech","da":"Danish","nl":"Dutch","en":"English","eo":"Esperanto","et":"Estonian","tl":"Filipino","fi":"Finnish","fr":"French","fy":"Frisian","gl":"Galician","ka":"Georgian","de":"German","el":"Greek","gu":"Gujarati","ht":"Haitian Creole","ha":"Hausa","haw":"Hawaiian","iw":"Hebrew","hi":"Hindi","hmn":"Hmong","hu":"Hungarian","is":"Icelandic","ig":"Igbo","id":"Indonesian","ga":"Irish","it":"Italian","ja":"Japanese","jw":"Javanese","kn":"Kannada","kk":"Kazakh","km":"Khmer","ko":"Korean","ku":"Kurdish (Kurmanji)","ky":"Kyrgyz","lo":"Lao","la":"Latin","lv":"Latvian","lt":"Lithuanian","lb":"Luxembourgish","mk":"Macedonian","mg":"Malagasy","ms":"Malay","ml":"Malayalam","mt":"Maltese","mi":"Maori","mr":"Marathi","mn":"Mongolian","my":"Myanmar (Burmese)","ne":"Nepali","no":"Norwegian","ps":"Pashto","fa":"Persian","pl":"Polish","pt":"Portuguese","ma":"Punjabi","ro":"Romanian","ru":"Russian","sm":"Samoan","gd":"Scots Gaelic","sr":"Serbian","st":"Sesotho","sn":"Shona","sd":"Sindhi","si":"Sinhala","sk":"Slovak","sl":"Slovenian","so":"Somali","es":"Spanish","su":"Sudanese","sw":"Swahili","sv":"Swedish","tg":"Tajik","ta":"Tamil","te":"Telugu","th":"Thai","tr":"Turkish","uk":"Ukrainian","ur":"Urdu","uz":"Uzbek","vi":"Vietnamese","cy":"Welsh","xh":"Xhosa","yi":"Yiddish","yo":"Yoruba","zu":"Zulu"}
+
+var LangsMap = map[string]string{"auto": "Automatic", "af": "Afrikaans", "sq": "Albanian", "ar": "Arabic", "hy": "Armenian", "az": "Azerbaijani", "eu": "Basque", "be": "Belarusian", "bn": "Bengali", "bs": "Bosnian", "bg": "Bulgarian", "ca": "Catalan", "ceb": "Cebuano", "ny": "Chichewa", "zh-cn": "Chinese Simplified", "zh-tw": "Chinese Traditional", "co": "Corsican", "hr": "Croatian", "cs": "Czech", "da": "Danish", "nl": "Dutch", "en": "English", "eo": "Esperanto", "et": "Estonian", "tl": "Filipino", "fi": "Finnish", "fr": "French", "fy": "Frisian", "gl": "Galician", "ka": "Georgian", "de": "German", "el": "Greek", "gu": "Gujarati", "ht": "Haitian Creole", "ha": "Hausa", "haw": "Hawaiian", "iw": "Hebrew", "hi": "Hindi", "hmn": "Hmong", "hu": "Hungarian", "is": "Icelandic", "ig": "Igbo", "id": "Indonesian", "ga": "Irish", "it": "Italian", "ja": "Japanese", "jw": "Javanese", "kn": "Kannada", "kk": "Kazakh", "km": "Khmer", "ko": "Korean", "ku": "Kurdish (Kurmanji)", "ky": "Kyrgyz", "lo": "Lao", "la": "Latin", "lv": "Latvian", "lt": "Lithuanian", "lb": "Luxembourgish", "mk": "Macedonian", "mg": "Malagasy", "ms": "Malay", "ml": "Malayalam", "mt": "Maltese", "mi": "Maori", "mr": "Marathi", "mn": "Mongolian", "my": "Myanmar (Burmese)", "ne": "Nepali", "no": "Norwegian", "ps": "Pashto", "fa": "Persian", "pl": "Polish", "pt": "Portuguese", "ma": "Punjabi", "ro": "Romanian", "ru": "Russian", "sm": "Samoan", "gd": "Scots Gaelic", "sr": "Serbian", "st": "Sesotho", "sn": "Shona", "sd": "Sindhi", "si": "Sinhala", "sk": "Slovak", "sl": "Slovenian", "so": "Somali", "es": "Spanish", "su": "Sudanese", "sw": "Swahili", "sv": "Swedish", "tg": "Tajik", "ta": "Tamil", "te": "Telugu", "th": "Thai", "tr": "Turkish", "uk": "Ukrainian", "ur": "Urdu", "uz": "Uzbek", "vi": "Vietnamese", "cy": "Welsh", "xh": "Xhosa", "yi": "Yiddish", "yo": "Yoruba", "zu": "Zulu"}
 
 var tkk string
 
-
 type TkRes struct {
-	Success bool `json:"success"`
-	Data map[string] interface {} `json:"data"`
+	Success bool                   `json:"success"`
+	Data    map[string]interface{} `json:"data"`
 }
 
-func tran(q string, tk string, to string, res *TkRes, wg *sync.WaitGroup) {
+func tran(q string, to string, res *TkRes, wg *sync.WaitGroup) {
 
 	defer func() {
 		err := recover()
@@ -29,8 +29,9 @@ func tran(q string, tk string, to string, res *TkRes, wg *sync.WaitGroup) {
 		}
 	}()
 
-	var  url = "https://translate.google.cn/translate_a/single?client=t&sl=zh-cn&tl="+to+"&hl="+to+"&dt=t&ie=UTF-8&oe=UTF-8&otf=1&ssel=0&tsel=0&kc=7&q="+ url.QueryEscape(q) +"&tk=" + tk
-
+	var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=" + to + "&dj=1&ie=UTF-8&oe=UTF-8&source=icon&dt=t&dt=bd&dt=qc&dt=rm&dt=ex&dt=at&dt=ss&dt=rw&dt=ld&q=" + url.QueryEscape(q)
+	//var  url = "https://translate.google.cn/translate_a/single?client=t&sl=zh-cn&tl="+to+"&hl="+to+"&dt=t&ie=UTF-8&oe=UTF-8&otf=1&ssel=0&tsel=0&kc=7&q="+ url.QueryEscape(q)
+	//+"&tk=" + tk
 	resp, err := http.Get(url)
 	resp.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36")
 	if err != nil {
@@ -47,7 +48,7 @@ func tran(q string, tk string, to string, res *TkRes, wg *sync.WaitGroup) {
 
 	fmt.Println(data)
 
-	r := regexp.MustCompile(`\"(.+?)\"`)
+	r := regexp.MustCompile(`\"trans\":\"(.+?)\"`)
 	if r.MatchString(data) {
 		data = r.FindStringSubmatch(data)[1]
 		res.Data[to] = data
@@ -59,9 +60,10 @@ func tran(q string, tk string, to string, res *TkRes, wg *sync.WaitGroup) {
 func Tran(q string, langs []string, res *TkRes) {
 
 	if len(langs) == 0 {
-		langs = []string{ "Automatic" }
+		langs = []string{"Automatic"}
 	}
-	tk := gt.GetTK(q, tkk)
+	//tk := gt.GetTK(q, tkk)
+	//fmt.Println("tk: ",tk)
 	var wg = &sync.WaitGroup{}
 	for _, item := range langs {
 		for key, val := range LangsMap {
@@ -70,15 +72,13 @@ func Tran(q string, langs []string, res *TkRes) {
 			}
 		}
 		wg.Add(1)
-		go tran(q,tk ,item, res, wg)
+		go tran(q, item, res, wg)
 	}
 	wg.Wait()
 
-
-
 }
 
-
 func init() {
-	tkk ,_ = gt.GetTKK()
+	tkk, _ = gt.GetTKK()
+	fmt.Println("tkk: ", tkk)
 }
